ff99613caaeb5f1401d3117cb33288f17555181d
ninom5 <nmilas00@fesb.hr>
2024-05-19 11:09:27 +0200

    chat app with multiple users on the same machine

---
+++ chat_app/src/Client.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.net.Socket;
+
+public class Client
+{
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientName;
+
+
+
+}
