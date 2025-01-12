c57e4f972593ab8e8643b8beac1da8a8974d12b6
ninom5 <nmilas00@fesb.hr>
2025-01-12 16:28:09 +0100

    added old project

---
+++ com.mycompany_krizic_kruzic_2_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/krizic_kruzic_2/Krizic_kruzic_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.krizic_kruzic_2;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class Krizic_kruzic_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
